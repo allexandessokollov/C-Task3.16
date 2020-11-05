f3cda3ec80983394561e2094f142b19ec073c994
allexandessokollov <allexandersokollov@gmail.com>
2020-11-05 09:43:14 +0300

    added task 3.16

---
+++ task3.16/task3.16/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task3._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
