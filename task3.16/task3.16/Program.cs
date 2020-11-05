fd66c0e107a915533bf6813bdcdd5cd5f246614a
allexandessokollov <allexandersokollov@gmail.com>
2020-11-05 10:31:49 +0300

    Class Gign created

--- task3.16/task3.16/Program.cs
+++ task3.16/task3.16/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http.Headers;
+using System.Reflection.Metadata;
 
 namespace task3._16
 {
@@ -6,7 +8,58 @@ namespace task3._16
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+                "Ut imperdiet elit a enim vulputate, non aliquam nisi pellentesque." +
+                " Lorem ipsum dolor sit amet, consectetur adipiscing elit." +
+                " Class aptent taciti sociosqu ad litora torquent per conubia nostra, " +
+                "per inceptos himenaeos. Nulla lacinia ligula a arcu pretium lobortis. " +
+                "Etiam ultricies hendrerit nunc.";
+
+           
+            
+            Sign[] sng = new Sign[s.Length];
+            for(int i = 0; i < sng.Length; i++)
+                sng[i] = new Sign();
+            Sign max;
+            
+            sng[0].setSign(s[0]); 
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                
+                if (alreadyExist(sng, s[i]))              
+                    sng[i].addCount(1);              
+                else
+                    sng[i].setSign(s[i]);
+            }
+
+            max = mostFrequently(sng);
+            Console.WriteLine(max.getChar() + "  Quantity is " + max.getCount());
+        }
+
+        static bool alreadyExist(Sign[] sng, char ch)
+        {
+            bool ret = true;
+            
+
+            for (int i = 0; i < sng.Length; i++)
+            {
+                if (sng[i].getChar() == ch)
+                    return ret;
+                
+            }
+            ret = false;
+            return ret;
+        }
+        static Sign mostFrequently(Sign[] sng)
+        {
+            Sign max = sng[1];
+            for (int i = 1; i < sng.Length; i++)
+            {
+                if (sng[i].getCount() > sng[i - 1].getCount())
+                    max = sng[i];
+            }
+            return max;
         }
     }
 }
---
+++ task3.16/task3.16/Sign.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace task3._16
+{
+    class Sign
+    {
+        private char ch;
+        private int count = 0;
+
+       
+
+        public char getChar()
+        {
+            return ch;
+        }
+        public int getCount()
+        {
+            return count;
+        }
+        public void addCount(int count)
+        {
+            this.count += count;
+        }
+        public void setSign(char ch)
+        {
+            this.ch = ch;
+        }
+    }
+}
